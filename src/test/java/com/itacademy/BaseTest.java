212984fe9b343c080b582345c00940f580183386
KseniyaFilonenko <filonenko_k@tut.by>
2024-06-09 19:59:22 +0300

    BaseTest is updated, testng.xml is added

--- src/test/java/com/itacademy/BaseTest.java
+++ src/test/java/com/itacademy/BaseTest.java
@@ -2,20 +2,26 @@ package com.itacademy;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 import java.time.Duration;
+
+import static com.codeborne.selenide.Configuration.browser;
 
 public class BaseTest {
     protected WebDriver driver;
 
     @BeforeTest
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:/Drivers/chromedriver.exe");
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("headless");
+        if (browser.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "C:/Drivers/chromedriver.exe");
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "C:/Drivers/geckodriver.exe");
+            driver = new FirefoxDriver();
+        }
         driver = new ChromeDriver();
         driver.get("https://belita-shop.by/");
         driver.manage().window().maximize();
