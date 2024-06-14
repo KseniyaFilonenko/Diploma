7b309eb4c1225cdeb313995555e09736c2091e98
KseniyaFilonenko <filonenko_k@tut.by>
2024-06-14 21:13:37 +0300

    WebsiteTest and ApiTest are updated with LOGGERs

--- src/test/java/com/itacademy/WebsiteTest.java
+++ src/test/java/com/itacademy/WebsiteTest.java
@@ -14,10 +14,14 @@ public class WebsiteTest extends BaseTest{
     public void verifyValidLogin(){
         HomePage homePage = new HomePage(driver);
         homePage.openUrl();
+        LOGGER.info("URL is opened");
         homePage.openLoginPage();
+        LOGGER.info("Login Page is opened");
         LoginPage loginPage = new LoginPage(driver);
         loginPage.typeLogin("filonenko_k@tut.by");
+        LOGGER.info("Login is filled with valid value");
         loginPage.typePassword("filonenko1234");
+        LOGGER.info("Password is filled with valid value");
         loginPage.clickEnterButton();
         Assert.assertEquals(loginPage.getSuccessLoginMessage(), loginPage.expectedSuccessLoginMessage());
     }
@@ -25,10 +29,14 @@ public class WebsiteTest extends BaseTest{
     public void verifyInvalidLogin(){
         HomePage homePage = new HomePage(driver);
         homePage.openUrl();
+        LOGGER.info("URL is opened");
         homePage.openLoginPage();
+        LOGGER.info("Login Page is opened");
         LoginPage loginPage = new LoginPage(driver);
         loginPage.typeLogin("k@tut.by");
+        LOGGER.info("Login is filled with invalid value");
         loginPage.typePassword("filonenko1234");
+        LOGGER.info("Password is filled with valid value");
         loginPage.clickEnterButton();
         Assert.assertEquals(loginPage.getFailedLoginMessage(), loginPage.expectedFailedLoginMessage());
     }
@@ -36,10 +44,14 @@ public class WebsiteTest extends BaseTest{
     public void verifyInvalidPassword(){
         HomePage homePage = new HomePage(driver);
         homePage.openUrl();
+        LOGGER.info("URL is opened");
         homePage.openLoginPage();
+        LOGGER.info("Login Page is opened");
         LoginPage loginPage = new LoginPage(driver);
         loginPage.typeLogin("filonenko_k@tut.by");
+        LOGGER.info("Login is filled with valid value");
         loginPage.typePassword("1234");
+        LOGGER.info("Password is filled with invalid value");
         loginPage.clickEnterButton();
         Assert.assertEquals(loginPage.getFailedLoginMessage(), loginPage.expectedFailedLoginMessage());
     }
@@ -47,8 +59,10 @@ public class WebsiteTest extends BaseTest{
     public void verifySearch(){
         HomePage homePage = new HomePage(driver);
         homePage.openUrl();
+        LOGGER.info("URL is opened");
         SearchPage searchPage = new SearchPage(driver);
         searchPage.typeSearchField("крем");
+        LOGGER.info("Search value is filled");
         searchPage.clickSearchIcon();
         searchPage.getFoundProductsList();
         searchPage.findProduct("Крем для лица ночной с муцином улитки");
